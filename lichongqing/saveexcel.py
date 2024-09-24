19a99140b4a04cae97e98b12b2490342fb1ebea6
a838831723 <96189270+a838831723@users.noreply.github.com>
2024-09-24 17:06:32 +0800

    Delete lichongqing directory

--- lichongqing/saveexcel.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import os
-def save(text,path,**kwargs):
-    if not os.path.exists(path):
-        os.makedirs(path)
-    writer = pd.ExcelWriter('{0}/{1}.xlsx'.format(path, text))
-    for i,k in kwargs.items():
-        k.to_excel(writer, sheet_name=i, index=False)
-    writer.close()
